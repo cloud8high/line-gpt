0ce8c6d5f47be6faf8088039d21a29de2262bcd9
Hayate.H <40209684+cloud8high@users.noreply.github.com>
2023-07-09 08:38:21 +0000

    不要な Lambda Layer を削除

--- line_gpt/line_gpt_stack.py
+++ line_gpt/line_gpt_stack.py
@@ -18,7 +18,7 @@ class LineGptStack(Stack):
         # 1. SecretManager の定義
         secrets = sm.Secret(
             self,
-            'ApiSecrets',
+            'LineGptSecrets',
             secret_object_value={
                 "LINE_MESSAGING_API_CHANNEL_ACCESS_TOKEN":  SecretValue.unsafe_plain_text("Must be changed after deployment - LINE_MESSAGING_API_CHANNEL_ACCESS_TOKEN"),
                 "LINE_MESSAGING_API_CHANNEL_SECRET":        SecretValue.unsafe_plain_text("Must be changed after deployment - LINE_MESSAGING_API_CHANNEL_SECRET"),
@@ -50,15 +50,8 @@ class LineGptStack(Stack):
                 type=dynamodb.AttributeType.STRING
             ),
         )
-        
-        # 3-1. Lambdaレイヤーの設定（AWS Parameters and Secrets Lambda Extension）
-        params_and_secrets = _lambda.ParamsAndSecretsLayerVersion.from_version(
-            _lambda.ParamsAndSecretsVersions.V1_0_103,
-            cache_size=500,
-            log_level=_lambda.ParamsAndSecretsLogLevel.DEBUG
-        )
-        
-        # 3-2. Lambda関数の設定
+                
+        # 3. Lambda関数の設定
         line_gpt_function = _lambda.Function(
             self,
             'LineGptFunction',
@@ -71,7 +64,6 @@ class LineGptStack(Stack):
                 'CHATS_TABLE_NAME':     chats_table.table_name,
                 'SECRETS_MANAGER_NAME': secrets.secret_name,
             },
-            params_and_secrets = params_and_secrets, # 定義したLambdaレイヤーを利用
         )
         
         # 4. API Gateway を追加 (3-2のLambdaをKickするREST-APIエンドポイント)
